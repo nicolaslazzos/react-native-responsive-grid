22750c8d05ff7207bc31ba0ab55af68c268c923d
nicolaslazzos <nicolaslazzos09@gmail.com>
2021-05-22 22:38:48 -0300

    adding peer deps

--- src/Grid.tsx
+++ src/Grid.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React from "react";
 import { View, ViewStyle, StyleProp } from "react-native";
 
 import { Col } from "./Col";
--- src/Provider.tsx
+++ src/Provider.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React from "react";
 import { View, StyleSheet, useWindowDimensions } from "react-native";
 
 export type OrientationType = "portrait" | "landscape";
